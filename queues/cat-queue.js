ca260a5d773e0f0bae6961930c43837fd33ee3a7
randy sartor <randal.sartor@gmail.com>
2019-01-25 16:37:22 -0500

    added pets

--- queues/cat-queue.js
+++ queues/cat-queue.js
@@ -13,6 +13,392 @@ const cats = [
     story: 'Thrown on the street'
   },
   {
+    imageURL:'http://catmylove.com/wp-content/uploads/2015/12/old_cat.jpeg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Grey with log hair.',
+    name: 'Robert',
+    sex: 'Male',
+    age: 36,
+    breed: 'Maine Coon',
+    story: 'Very old, previous owner passed away'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'White',
+    name: 'Sante',
+    sex: 'Male',
+    age: 5,
+    breed: 'Siamese',
+    story: 'Found at a walmart'
+  },
+  {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange tabby cat',
+    name: 'Pia',
+    sex: 'Female',
+    age: 4,
+    breed: 'Unkown',
+    story: 'Unkown'
+  },{
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  },
+  {
     imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
     imageDescription: 'Grey with log hair.',
     name: 'Robert',
--- queues/dog-queue.js
+++ queues/dog-queue.js
@@ -38,6 +38,358 @@ const dogs = [
     age: 11,
     breed: 'Irish Pointer',
     story: 'Donated to a shelter'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },{
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },{
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },{
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },{
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },{
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },{
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },{
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
+  },{
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+    name: 'Zeus',
+    sex: 'Male',
+    age: 3,
+    breed: 'Golden Retriever',
+    story: 'Owner Passed away'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Grey pitbul',
+    name: 'Thanos',
+    sex: 'Male',
+    age: 3,
+    breed: 'Pitbull',
+    story: 'abandoned by owner'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Little black dog',
+    name: 'Mindy',
+    sex: 'Female',
+    age: 3,
+    breed: 'Corgi',
+    story: 'Lost in a park'
+  },
+  {
+    imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+    imageDescription: 'Dirty white dog',
+    name: 'Mr. Pete',
+    sex: 'Male',
+    age: 11,
+    breed: 'Irish Pointer',
+    story: 'Donated to a shelter'
   }
 ];
 
